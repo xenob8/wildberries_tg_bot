5d6d1d57f7d838fb703c7c85bbe71c472bbc23d7
pletn <pletnyovski@gmail.com>
2023-11-25 03:26:07 +0300

    delete unused file

--- handlers/product_card.py
+++
@@ -1,7 +0,0 @@
-from aiogram.utils.markdown import hide_link
-
-
-def get_card(link, title, start_price, last_price, diff_price, treshhold):
-    return f"{hide_link(link)}Название товара: {title}\n" \
-           f"Изначальная цена товара: {start_price}\nПоследняя измененная цена товара: {last_price}\n" \
-           f"Разница в цене: {diff_price}\nТекущий порог оповещения: {treshhold}\n"
