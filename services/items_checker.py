8bd01807dfd20b7c8a4deb8cb5e8e3601f52375c
lupperant <nefedovatata01@gmail.com>
2023-11-27 00:42:56 +0300

    still not working notifier but almost done

--- services/items_checker.py
+++ services/items_checker.py
@@ -1,10 +1,8 @@
 from api.api_service import get_product
-from db import product_service
-from db.dto.ProductUpdateDto import ProductUpdateDto
 
+from db.dto.ProductUpdateDto import ProductUpdateDto
 from db.product_service import ProductService
-from notifier import Notifier
-from typing import TypedDict
+
 import time
 
 from services import notifier
@@ -12,33 +10,37 @@ from services import notifier
 
 async def check_price_change(item_art):
     history = await get_product(item_art)
-    price = history[0]['salePriceU'] / 100
-    return price
+    if history:
+        price = history[0]['salePriceU'] / 100
+        return price
 
 
 async def check_availability(item_art):
     history = await get_product(item_art)
-    size = history[0]['sizes']
-    availability = False
-    if len(size['stocks']):
-        availability = True
-    return availability
+    if history:
+        availability = False
+        for size in history[0]['sizes']:
+            if len(size['stocks']):
+                availability = True
+                break
+        return availability
 
 
-async def update(engine, session, freq=60 * 5):
-    product_service = ProductService(engine)
-    products_from_bd = product_service.get_all_product()  ##продукты из бд
-    for product in products_from_bd:
-        product_art, product_price, product_avail = product.number, product.price, product.availability
-        availability_from_api = await check_availability(product_art)
-        price_from_api = await check_price_change(product_art)
-        if product_price != price_from_api:
-            new_product = ProductUpdateDto(price=price_from_api)
-            product_service.patch_product(product_art, new_product, session)
-            notifier.notify(product_art, price_from_api, product_price)
-        if product_avail != availability_from_api:
-            new_product = ProductUpdateDto(availability=availability_from_api)
-            product_service.patch_product(product_art, new_product, session)
-            notifier.notify(product_art, availability_from_api, product_avail)
-    time.sleep(freq)
+async def update(message, product_service: ProductService, freq=60):
+    while True:
+        products_from_bd = product_service.get_all_product()  ##продукты из бд
+        for product in products_from_bd:
+            product_art, product_price, product_avail = product.number, product.price, product.availability
+            availability_from_api = await check_availability(product_art)
+            price_from_api = await check_price_change(product_art)
+            if product_price != price_from_api:
+                new_product = ProductUpdateDto(price=price_from_api)
+                product_service.patch_product(product_art, new_product)
+                notifier.notify(product_art, price_from_api, product_service, message)
+            if product_avail != availability_from_api:
+                new_product = ProductUpdateDto(availability=availability_from_api)
+                product_service.patch_product(product_art, new_product)
+                notifier.notify_avail(product_art, availability_from_api, product_service, message)
+        time.sleep(freq)
+        print("updating")
 
--- services/notifier.py
+++ services/notifier.py
@@ -1,25 +1,22 @@
-from typing import TypedDict
-import time
+from db.product_service import ProductService
+from aiogram import Bot
 
 
-class UserItem(TypedDict):
-    telegram_id: str
-    last_price: int
+def notify(item_art, new_val: float, product_service: ProductService, bot: Bot):
+    positions = product_service.get_users_of_product(item_art)
+    for item in positions:
+        if item.start_price:
+            if item.start_price > new_val:
+                bot.send_message(item.user_telegram_id, "Цена понизилась")
+            elif item.start_price < new_val:
+                bot.send_message(item.user_telegram_id, "Цена повысилась")
 
 
-class Notifier():
-    def __init__(self):
-        pass
-
-    def update(self, check_freq=5 * 60):
-        print(1)
-
-
-def notify(item_art, new_val, last_val):
-    pass
-
-
-
-def notify(item_art, new_availability, last_availability):
-
-    pass
+def notify_avail(item_art, new_availability, product_service: ProductService, bot: Bot):
+    positions = product_service.get_users_of_product(item_art)
+    for item in positions:
+        if item:
+            if new_availability:
+                bot.send_message(item.user_telegram_id, "Товар появился в наличии")
+            else:
+                bot.send_message(item.user_telegram_id, "Товар не в наличии")
