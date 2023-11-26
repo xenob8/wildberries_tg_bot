b481f6151319cf0b67d5d4f719e7250be251de59
lupperant <nefedovatata01@gmail.com>
2023-11-26 17:57:01 +0300

    update not working yet

--- services/items_checker.py
+++ services/items_checker.py
@@ -1,47 +1,44 @@
 from api.api_service import get_product
 from db import product_service
+from db.dto.ProductUpdateDto import ProductUpdateDto
+
 from db.product_service import ProductService
 from notifier import Notifier
 from typing import TypedDict
 import time
 
-
-class Item(TypedDict):
-    art: str
-    last_price: int
+from services import notifier
 
 
-async def check_price_change(item_art, last_price):
+async def check_price_change(item_art):
     history = await get_product(item_art)
     price = history[0]['salePriceU'] / 100
-    if abs(price - last_price) != 0:
-        return price
+    return price
 
 
-async def check_availability(item_art, last_availability):
+async def check_availability(item_art):
     history = await get_product(item_art)
     size = history[0]['sizes']
     availability = False
     if len(size['stocks']):
         availability = True
-    return availability != last_availability
+    return availability
 
 
-class ItemsChecker():
-    def __init__(self, notifier: Notifier):
-        self.items: list[Item] = []
-        self.notifier = notifier
+async def update(engine, session, freq=60 * 5):
+    product_service = ProductService(engine)
+    products_from_bd = product_service.get_all_product()  ##продукты из бд
+    for product in products_from_bd:
+        product_art, product_price, product_avail = product.number, product.price, product.availability
+        availability_from_api = await check_availability(product_art)
+        price_from_api = await check_price_change(product_art)
+        if product_price != price_from_api:
+            new_product = ProductUpdateDto(price=price_from_api)
+            product_service.patch_product(product_art, new_product, session)
+            notifier.notify(product_art, price_from_api, product_price)
+        if product_avail != availability_from_api:
+            new_product = ProductUpdateDto(availability=availability_from_api)
+            product_service.patch_product(product_art, new_product, session)
+            notifier.notify(product_art, availability_from_api, product_avail)
+    time.sleep(freq)
 
-    def check_items(self):
-        for item in self.items:
-            item_art, last_price = item["art"], item["last_price"]
-            new_price = check_price_change(item_art, last_price)
-            if new_price:
-                self.notifier.notify(item_art, new_price)
-
-    def update(self, freq=60*5):
-        product_service = ProductService()
-        products = product_service.get_all_product()
-        ItemsChecker()
-        check_items()
-        time.sleep(freq)
--- services/notifier.py
+++ services/notifier.py
@@ -1,19 +1,25 @@
 from typing import TypedDict
 import time
 
+
 class UserItem(TypedDict):
     telegram_id: str
     last_price: int
-
-
 
 
 class Notifier():
     def __init__(self):
         pass
 
-    def update(self, check_freq=5*60):
+    def update(self, check_freq=5 * 60):
         print(1)
 
-    def notify(self, item_art, new_val):
-        pass
+
+def notify(item_art, new_val, last_val):
+    pass
+
+
+
+def notify(item_art, new_availability, last_availability):
+
+    pass
